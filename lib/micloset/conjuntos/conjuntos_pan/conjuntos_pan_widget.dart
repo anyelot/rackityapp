fd1bee5aa1a3d192e43f4ce72049ff5a80a60e8c
anyelot <81717749+anyelot@users.noreply.github.com>
2023-05-14 21:10:14 -0500

    Frontend update

--- lib/micloset/conjuntos/conjuntos_pan/conjuntos_pan_widget.dart
+++ lib/micloset/conjuntos/conjuntos_pan/conjuntos_pan_widget.dart
@@ -8,6 +8,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
 import 'conjuntos_pan_model.dart';
 export 'conjuntos_pan_model.dart';
 
@@ -50,9 +51,9 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
         decoration: BoxDecoration(
           gradient: LinearGradient(
             colors: [Color(0xFF4AC7AC), Color(0xFF4EDBBC)],
-            stops: [0.0, 1.0],
-            begin: AlignmentDirectional(1.0, -1.0),
-            end: AlignmentDirectional(-1.0, 1.0),
+            stops: [0, 1],
+            begin: AlignmentDirectional(1, -1),
+            end: AlignmentDirectional(-1, 1),
           ),
         ),
         child: Column(
@@ -60,10 +61,10 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              width: 10.0,
+              width: 10,
               child: Divider(
-                height: 25.0,
-                thickness: 1.0,
+                height: 25,
+                thickness: 1,
                 color: Color(0x00EEEEEE),
               ),
             ),
@@ -72,7 +73,7 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Align(
-                  alignment: AlignmentDirectional(0.0, 0.0),
+                  alignment: AlignmentDirectional(0, 0),
                   child: Text(
                     'Mi clóset',
                     textAlign: TextAlign.start,
@@ -80,29 +81,29 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
                       'Poppins',
                       color: Color(0xFF0F5C4B),
                       fontWeight: FontWeight.w600,
-                      fontSize: 30.0,
+                      fontSize: 30,
                       fontStyle: FontStyle.normal,
                     ),
                   ),
                 ),
                 SizedBox(
-                  height: 50.0,
+                  height: 50,
                   child: VerticalDivider(
-                    width: 150.0,
-                    thickness: 2.0,
+                    width: 150,
+                    thickness: 2,
                     color: Color(0x00EEEEEE),
                   ),
                 ),
                 FlutterFlowIconButton(
                   borderColor: Color(0xFFC8C8C8),
-                  borderRadius: 30.0,
-                  borderWidth: 1.0,
-                  buttonSize: 45.0,
+                  borderRadius: 30,
+                  borderWidth: 1,
+                  buttonSize: 45,
                   fillColor: Colors.white,
                   icon: Icon(
                     Icons.face_rounded,
                     color: Color(0xFFFCA379),
-                    size: 20.0,
+                    size: 20,
                   ),
                   onPressed: () async {
                     context.pushNamed(
@@ -122,9 +123,9 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
               mainAxisSize: MainAxisSize.max,
               children: [
                 SizedBox(
-                  height: 60.0,
+                  height: 60,
                   child: VerticalDivider(
-                    thickness: 1.0,
+                    thickness: 1,
                     color: Color(0x00EEEEEE),
                   ),
                 ),
@@ -143,28 +144,27 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
                   },
                   text: 'Prendas',
                   options: FFButtonOptions(
-                    width: 100.0,
-                    height: 45.0,
-                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                    iconPadding:
-                        EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                    width: 100,
+                    height: 45,
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
                     color: Color(0x004B39EF),
                     textStyle: FlutterFlowTheme.of(context).titleSmall.override(
                           fontFamily: 'Poppins',
                           color: Color(0xFF0F5C54),
-                          fontSize: 15.0,
+                          fontSize: 15,
                         ),
                     borderSide: BorderSide(
                       color: Colors.transparent,
-                      width: 1.0,
+                      width: 1,
                     ),
-                    borderRadius: BorderRadius.circular(8.0),
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
                 SizedBox(
-                  height: 60.0,
+                  height: 60,
                   child: VerticalDivider(
-                    thickness: 1.0,
+                    thickness: 1,
                     color: Color(0x00EEEEEE),
                   ),
                 ),
@@ -174,50 +174,47 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
                   },
                   text: 'Conjuntos',
                   options: FFButtonOptions(
-                    width: 100.0,
-                    height: 45.0,
-                    padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                    iconPadding:
-                        EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                    width: 100,
+                    height: 45,
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
                     color: Color(0xFFB3FFEE),
                     textStyle: FlutterFlowTheme.of(context).titleSmall.override(
                           fontFamily: 'Poppins',
                           color: Color(0xFF0F5C54),
-                          fontSize: 15.0,
+                          fontSize: 15,
                         ),
                     borderSide: BorderSide(
                       color: Color(0xFF7DFFE3),
-                      width: 1.0,
+                      width: 1,
                     ),
-                    borderRadius: BorderRadius.circular(18.0),
+                    borderRadius: BorderRadius.circular(18),
                   ),
                 ),
               ],
             ),
             Container(
               width: 389.7,
-              height: 700.0,
               decoration: BoxDecoration(
                 color: FlutterFlowTheme.of(context).secondaryBackground,
-                borderRadius: BorderRadius.circular(45.0),
+                borderRadius: BorderRadius.circular(45),
               ),
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 children: [
                   Divider(
-                    height: 10.0,
-                    thickness: 1.0,
+                    height: 10,
+                    thickness: 1,
                     color: Color(0x00EEEEEE),
                   ),
                   Align(
-                    alignment: AlignmentDirectional(0.0, -0.95),
+                    alignment: AlignmentDirectional(0, -0.95),
                     child: Row(
                       mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         Padding(
-                          padding: EdgeInsetsDirectional.fromSTEB(
-                              40.0, 0.0, 0.0, 0.0),
+                          padding: EdgeInsetsDirectional.fromSTEB(40, 0, 0, 0),
                           child: Text(
                             'Filtrar',
                             style: FlutterFlowTheme.of(context)
@@ -225,19 +222,19 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
                                 .override(
                                   fontFamily: 'Poppins',
                                   color: Color(0xFF0F5C54),
-                                  fontSize: 20.0,
+                                  fontSize: 20,
                                 ),
                           ),
                         ),
                         FlutterFlowIconButton(
                           borderColor: Colors.transparent,
-                          borderRadius: 30.0,
-                          borderWidth: 1.0,
-                          buttonSize: 40.0,
+                          borderRadius: 30,
+                          borderWidth: 1,
+                          buttonSize: 40,
                           icon: Icon(
                             Icons.keyboard_arrow_down_outlined,
                             color: FlutterFlowTheme.of(context).primaryText,
-                            size: 25.0,
+                            size: 25,
                           ),
                           onPressed: () {
                             print('IconButton pressed ...');
@@ -248,143 +245,168 @@ class _ConjuntosPanWidgetState extends State<ConjuntosPanWidget>
                   ),
                   Container(
                     width: 395.9,
-                    height: 389.0,
+                    height: 389,
                     decoration: BoxDecoration(
                       color: Colors.white,
                     ),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(5, 0, 5, 0),
+                      child: GridView(
+                        padding: EdgeInsets.zero,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 10,
+                          childAspectRatio: 1,
+                        ),
+                        scrollDirection: Axis.vertical,
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              color: Color(0x00FFFFFF),
+                              borderRadius: BorderRadius.circular(20),
+                              border: Border.all(
+                                color: Color(0xFF0F5C54),
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                   Container(
-                    width: 398.4,
-                    height: 138.6,
                     decoration: BoxDecoration(
                       color: Color(0xFF41B69E),
-                      borderRadius: BorderRadius.circular(40.0),
+                      borderRadius: BorderRadius.circular(40),
                     ),
                     child: Align(
-                      alignment: AlignmentDirectional(0.0, -0.65),
+                      alignment: AlignmentDirectional(0, -0.65),
                       child: Row(
                         mainAxisSize: MainAxisSize.max,
                         children: [
                           SizedBox(
-                            height: 75.0,
+                            height: 75,
                             child: VerticalDivider(
-                              width: 20.0,
-                              thickness: 1.0,
+                              width: 20,
+                              thickness: 1,
                               color: Color(0x00EEEEEE),
                             ),
                           ),
                           FlutterFlowIconButton(
                             borderColor: Color(0xFFC8C8C8),
-                            borderRadius: 30.0,
-                            borderWidth: 1.0,
-                            buttonSize: 60.0,
+                            borderRadius: 30,
+                            borderWidth: 1,
+                            buttonSize: 60,
                             fillColor: Colors.white,
                             icon: Icon(
                               Icons.all_inbox,
                               color: Color(0xFFFCA379),
-                              size: 30.0,
+                              size: 30,
                             ),
                             onPressed: () {
                               print('IconButton pressed ...');
                             },
                           ),
                           SizedBox(
-                            height: 75.0,
+                            height: 75,
                             child: VerticalDivider(
-                              width: 10.0,
-                              thickness: 1.0,
+                              width: 10,
+                              thickness: 1,
                               color: Color(0x00EEEEEE),
                             ),
                           ),
                           FlutterFlowIconButton(
                             borderColor: Color(0xFFC8C8C8),
-                            borderRadius: 30.0,
-                            borderWidth: 1.0,
-                            buttonSize: 60.0,
+                            borderRadius: 30,
+                            borderWidth: 1,
+                            buttonSize: 60,
                             fillColor: Colors.white,
                             icon: Icon(
                               Icons.auto_awesome,
                               color: Color(0xFF4AC7AC),
-                              size: 30.0,
+                              size: 30,
                             ),
                             onPressed: () {
                               print('IconButton pressed ...');
                             },
                           ),
                           SizedBox(
-                            height: 75.0,
+                            height: 75,
                             child: VerticalDivider(
-                              width: 10.0,
-                              thickness: 1.0,
+                              width: 10,
+                              thickness: 1,
                               color: Color(0x00EEEEEE),
                             ),
                           ),
                           FlutterFlowIconButton(
                             borderColor: Color(0xFFC8C8C8),
-                            borderRadius: 30.0,
-                            borderWidth: 1.0,
-                            buttonSize: 70.0,
+                            borderRadius: 30,
+                            borderWidth: 1,
+                            buttonSize: 70,
                             fillColor: Colors.white,
                             icon: Icon(
                               Icons.camera_enhance_outlined,
                               color: Color(0xFFFCA379),
-                              size: 45.0,
+                              size: 45,
                             ),
                             onPressed: () {
                               print('IconButton pressed ...');
                             },
                           ),
                           SizedBox(
-                            height: 75.0,
+                            height: 75,
                             child: VerticalDivider(
-                              width: 10.0,
-                              thickness: 1.0,
+                              width: 10,
+                              thickness: 1,
                               color: Color(0x00EEEEEE),
                             ),
                           ),
                           FlutterFlowIconButton(
                             borderColor: Color(0xFFC8C8C8),
-                            borderRadius: 30.0,
-                            borderWidth: 1.0,
-                            buttonSize: 60.0,
+                            borderRadius: 30,
+                            borderWidth: 1,
+                            buttonSize: 60,
                             fillColor: Colors.white,
                             icon: Icon(
                               Icons.calendar_today_sharp,
                               color: Color(0xFF4AC7AC),
-                              size: 30.0,
+                              size: 30,
                             ),
                             onPressed: () {
                               print('IconButton pressed ...');
                             },
                           ),
                           SizedBox(
-                            height: 75.0,
+                            height: 75,
                             child: VerticalDivider(
-                              width: 10.0,
-                              thickness: 1.0,
+                              width: 10,
+                              thickness: 1,
                               color: Color(0x00EEEEEE),
                             ),
                           ),
                           FlutterFlowIconButton(
                             borderColor: Color(0xFFC8C8C8),
-                            borderRadius: 30.0,
-                            borderWidth: 1.0,
-                            buttonSize: 60.0,
+                            borderRadius: 30,
+                            borderWidth: 1,
+                            buttonSize: 60,
                             fillColor: Colors.white,
                             icon: Icon(
                               Icons.search_sharp,
                               color: Color(0xFF4AC7AC),
-                              size: 30.0,
+                              size: 30,
                             ),
                             onPressed: () {
                               print('IconButton pressed ...');
                             },
                           ),
                           SizedBox(
-                            height: 75.0,
+                            height: 75,
                             child: VerticalDivider(
-                              width: 20.0,
-                              thickness: 1.0,
+                              width: 20,
+                              thickness: 1,
                               color: Color(0x00EEEEEE),
                             ),
                           ),
